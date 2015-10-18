e706ee692c3dca1cbbc6141084148bddf9b1fca5
AndresZamora <andru1291@gmail.com>
2015-10-18 14:33:01 +0200

    Lab4 PS1 Completo, validaciones PHP en el servidor

--- registrar.php
+++ registrar.php
@@ -17,7 +17,13 @@
 		exit;
 	}
 	
-	if(!empty($_GET['interes'])){
+	if (filter_var($_GET['email'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^([a-zA-Z])+(\d{3})+\@ikasle.ehu.e(u)?s$/")))&&
+		filter_var($_GET['firstname'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+\s[a-zA-ZñÑáéíóúÁÉÍÓÚ]+\s[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/")))&&
+		filter_var($_GET['password'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^([a-zA-Z\d]){6,}$/")))&&
+		filter_var($_GET['telephone'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^(\d{9})$/")))&&
+		filter_var($_GET['especialidad'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z ñÑáéíóúÁÉÍÓÚ]+$/")))&&
+		filter_var($_GET['interes'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z ñÑáéíóúÁÉÍÓÚ]+$/")))		
+		){
 		if (!mysqli_query($enlace,"INSERT INTO usuario(Email,Nombre,Password,Telefono,Especialidad,Interes) 
 			VALUES('".$_GET['email']."','".$_GET['firstname']."','".$_GET['password']."',".$_GET['telephone'].",'".$_GET['especialidad']."','".$_GET['interes']."' )")) {
 			echo "<h2>ERROR: Falló en la inserción en la tabla</h2>";
@@ -25,7 +31,14 @@
 			echo ("<h2>EXITO: Se ha insertado correctamente los datos en la tabla</h2><br>
 					<a href='VerUsuarios.php'>Usuarios Insertados</a>");
 		}
-	}else{
+	}else if (filter_var($_GET['email'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^([a-zA-Z])+(\d{3})+\@ikasle.ehu.e(u)?s$/")))&&
+		filter_var($_GET['firstname'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+\s[a-zA-ZñÑáéíóúÁÉÍÓÚ]+\s[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/")))&&
+		filter_var($_GET['password'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^([a-zA-Z\d]){6,}$/")))&&
+		filter_var($_GET['telephone'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^(\d{9})$/")))&&
+		filter_var($_GET['especialidad'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z ñÑáéíóúÁÉÍÓÚ]+$/")))
+		){
+		$message = "<h2>Mensaje: Todo Correcto</h2>";
+		echo $message;
 		if (!mysqli_query($enlace,"INSERT INTO usuario(Email,Nombre,Password,Telefono,Especialidad) 
 			VALUES('".$_GET['email']."','".$_GET['firstname']."','".$_GET['password']."',".$_GET['telephone'].",'".$_GET['especialidad']."' )")) {
 			echo "<h2>ERROR: Falló en la inserción en la tabla</h2>";
@@ -33,6 +46,41 @@
 			echo ("<h2>EXITO: Se ha insertado correctamente los datos en la tabla</h2><br>
 					<a href='VerUsuarios.php'>Usuarios Insertados</a>");
 		}
+	}else{
+		$message = "<h2>ERROR: Algún campo es incorrecto</h2></br>";
+		echo $message;
+		if(filter_var($_GET['email'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^([a-zA-Z])+(\d{3})+\@ikasle.ehu.e(u)?s$/")))){
+			echo "<h2><font color=green>Mensaje: Email Correcto</font></h2></br>";
+		}else{
+			echo "<h2><font color=red>ERROR: Email incorrecto</font></h2></br>";
+		}
+		if(filter_var($_GET['firstname'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+\s[a-zA-ZñÑáéíóúÁÉÍÓÚ]+\s[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/")))){
+			echo "<h2><font color=green>Mensaje: Nombre Correcto</font></h2></br>";
+		}else{
+			echo "<h2><font color=red>ERROR: Nombre incorrecto</font></h2></br>";
+		}
+		if(filter_var($_GET['password'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^([a-zA-Z\d]){6,}$/")))){
+			echo "<h2><font color=green>Mensaje: Password Correcto</font></h2></br>";
+		}else{
+			echo "<h2><font color=red>ERROR: Password incorrecto</font></h2></br>";
+		}
+		if(filter_var($_GET['telephone'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^(\d{9})$/")))){
+			echo "<h2><font color=green>Mensaje: Telefono Correcto</font></h2></br>";
+		}else{
+			echo "<h2><font color=red>ERROR: Telefono incorrecto</font></h2></br>";
+		}
+		if(filter_var($_GET['especialidad'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z ñÑáéíóúÁÉÍÓÚ]+$/")))){
+			echo "<h2><font color=green>Mensaje: Especialidad Correcto</font></h2></br>";
+		}else{
+			echo "<h2><font color=red>ERROR: Especialidad incorrecto</font></h2></br>";
+		}
+		if(!empty($_GET['interes'])){
+			if(filter_var($_GET['interes'], FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z ñÑáéíóúÁÉÍÓÚ]+$/")))){
+				echo "<h2><font color=green>Mensaje: Interes Correcto</font></h2></br>";
+			}else{
+				echo "<h2><font color=red>ERROR: Interes incorrecto</font></h2></br>";
+			}
+		}	
 	}
 
 	mysqli_close($enlace);
